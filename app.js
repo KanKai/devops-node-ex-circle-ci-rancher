f0cfd36d54502e69075b1a5c42899c43c50e0a60
Jeerawat Keawsawat <gukaihero@gmail.com>
2019-12-08 17:07:08 +0700

    add apmserver

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var apm = require('elastic-apm-node').start({
   // Use if APM Server requires a token
   secretToken: '',
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'elasticsearch'
+  serverUrl: 'elasticsearchamp'
 })
 var express = require('express');
 var path = require('path');
